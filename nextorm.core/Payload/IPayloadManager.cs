96a61508edc09a86efea74d2abc60526d924871a
Алексей Ширшов <@VAIO.corp.kelix.ru>
2023-10-29 19:49:54 +0300

    refactor payload

---
+++ nextorm.core/Payload/IPayloadManager.cs
@@ -0,0 +1,11 @@
+namespace nextorm.core;
+
+public interface IPayloadManager
+{
+    bool RemovePayload<T>() where T : class, IPayload;
+    bool TryGetPayload<T>(out T? payload) where T : class, IPayload;
+    bool TryGetNotNullPayload<T>(out T? payload) where T : class, IPayload;
+    T GetNotNullOrAddPayload<T>(Func<T> factory) where T : class, IPayload;
+    T? GetOrAddPayload<T>(Func<T?> factory) where T : class, IPayload;
+    T? AddOrUpdatePayload<T>(Func<T?> factory) where T : class, IPayload;
+}
